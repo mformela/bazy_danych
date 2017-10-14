873ddb2a2ad59c4cb4cd013d656cd0b2dfbad2e2
Marta Formela <formela.marta@gmail.com>
2017-10-14 14:32:52 +0200

    b

---
+++ Airlines/Airlines/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Airlines.Startup))]
+namespace Airlines
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
